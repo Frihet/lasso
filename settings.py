bcda9b726fb0548e433b5869e3646f2b10363aa3
Egil Moeller <egil.moller@freecode.no>
2010-09-17 14:00:35 +0200

    Made the django admin site translatable and provided a (very bad) german  translation

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for lasso project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -25,7 +26,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de'
 
 SITE_ID = 1
 
@@ -69,15 +70,18 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
     'lasso.lasso_warehouse',
     'lasso.lasso_customer',
     'lasso.lasso_warehandling',
+    'lasso.i18next',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'rosetta',
 )
