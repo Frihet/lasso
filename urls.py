7d79678a46d45119aa5a4c3697db10850e367dfb
Egil Moeller <egil.moller@freecode.no>
2010-09-21 14:19:54 +0200

    Integrated the cost log better with the rest of the site

--- urls.py
+++ urls.py
@@ -7,21 +7,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^lasso/', include('lasso.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
+    (r'^admin/lasso_warehandling/costlog/', include('lasso_warehandling.urls.costlog')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-
     (r'^rosetta/', include('rosetta.urls')),
-
     (r'^i18n/', include('django.conf.urls.i18n')),
-
-    (r'^costlog/', include('lasso_warehandling.urls.costlog')),
-
     (r'^' + settings.MEDIA_URL + '(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
