895ce93f97ed59c8648deee5e4af8731ced66e68
Egil Moeller <egil.moller@freecode.no>
2010-10-18 10:36:13 +0200

    Moved all stuff upp to top level url

--- urls.py
+++ urls.py
@@ -8,11 +8,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    (r'^admin/lasso_warehandling/costlog/', include('lasso_warehandling.urls.costlog')),
-    (r'^admin/lasso_warehouse/overview/', include('lasso_warehouse.urls.overview')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    (r'^lasso_warehandling/costlog/', include('lasso_warehandling.urls.costlog')),
+    (r'^lasso_warehouse/overview/', include('lasso_warehouse.urls.overview')),
+    (r'^doc/', include('django.contrib.admindocs.urls')),
     (r'^rosetta/', include('rosetta.urls')),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^' + settings.MEDIA_URL[1:] + '(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    (r'^', include(admin.site.urls)),
 )
