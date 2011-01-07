21dcc34c3c1c7c2de88b5ab0e6ecae932d6ba934
root <root@lasso.fls-furness.ch>
2011-01-07 09:27:46 +0100

    DEPLOY: Server name in admin media prefix

--- settings.py
+++ settings.py
@@ -47,7 +47,8 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = 'http://217.193.154.12/media/admin/'
+ADMIN_MEDIA_PREFIX = 'http://www.lasso.fls-furness.ch/media/admin/'
+#ADMIN_MEDIA_PREFIX = 'http://217.193.154.12/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '+dacm+#gpsk=l8(yvnpb*=pe1=n5^4g%dk$_a8=&b#)c*iah(l'
