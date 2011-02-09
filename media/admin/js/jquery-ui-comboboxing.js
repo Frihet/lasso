2511c2fa6166c891649c52ea3d3b4ff73e89d6ff
Egil Moeller <egil.moller@freecode.no>
2011-02-09 11:43:28 +0100

    Bugfix for IE

--- media/admin/js/jquery-ui-comboboxing.js
+++ media/admin/js/jquery-ui-comboboxing.js
@@ -36,13 +36,13 @@
 					},
 					change: function( event, ui ) {
 						if ( !ui.item ) {
-							var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" ),
-								valid = false;
-							select.children( "option" ).each(function() {
-								if ( this.value.match( matcher ) ) {
+						        var value = $(this).val();
+							var valid = false;
+							select.children( "option" ).each(function() {							  
+							  if ( $(this).text() == value ) {							   
 									this.selected = valid = true;
 									return false;
-								}
+							 }
 							});
 							if ( !valid ) {
 								// remove invalid value, as it didn't match anything
