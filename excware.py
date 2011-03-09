9c23ced5e7cac8c3d0f076b0505b2a156f110d18
Egil Moeller <egil.moller@freecode.no>
2011-03-09 11:37:56 +0100

    Better exception handling

--- excware.py
+++ excware.py
@@ -3,7 +3,9 @@ import settings
 class ExcWare(object):
     def process_exception(self, request, exception):
         if settings.DEBUG_IN_TERMINAL:
-            import pdb, sys
+            import pdb, sys, traceback
             sys.last_traceback = sys.exc_info()[2]
+            print "================================================================================"
+            traceback.print_exc()
             pdb.pm()
         return None
